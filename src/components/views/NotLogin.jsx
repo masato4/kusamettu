853d48274f64fada0db2fbb21ca62fe0e2de631d
saki000 <hangchengtengqi@gmail.com>
2022-09-07 11:46:09 +0900

    fix: ログイン機能をついか

--- src/components/views/NotLogin.jsx
+++ src/components/views/NotLogin.jsx
@@ -1,24 +1,21 @@
+import { Button } from "@mantine/core";
+import { LoginHeader } from "../parts/LoginHeader";
 
-import { Button } from '@mantine/core';
-import { LoginHeader } from '../parts/LoginHeader';
-
-export const NotLogin = ({login}) => {
+export const NotLogin = ({ login }) => {
   return (
     <>
-      <div className='image max-w-screen max-h-screen text-center' >
+      <div className="image max-w-screen max-h-screen text-center">
         {/* <img src={BgImg} className="object-fill w-full absolute -z-10 top-0" alt="" /> */}
         <LoginHeader />
-        <div className='sm:text-9xl text-6xl text-white w-screen grid grid-cols-1 my-24 place-content-center '>
-          <div className='text-center break-normal'>
+        <div className="sm:text-9xl text-6xl text-white w-screen grid grid-cols-1 my-24 place-content-center ">
+          <div className="text-center break-normal">
             みんな草
             <br />
             はやしてる？
           </div>
         </div>
-        <Button color="green" size="xl" style={{width: 400, }}>
-          <div className=''>
-            草を生やしに行く
-          </div>
+        <Button onClick={login} color="green" size="xl" style={{ width: 400 }}>
+          <div className="">草を生やしに行く</div>
         </Button>
       </div>
     </>
@@ -26,4 +23,3 @@ export const NotLogin = ({login}) => {
 };
 
 export default NotLogin;
-
